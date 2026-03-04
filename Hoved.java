e32b3edd986eda41c2fc37933cace4f9d4a03323
Aleksander <Alringstad@outlook.com>
2026-03-04 13:18:20 +0100

    endringer

--- Hoved.java
+++ Hoved.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 class Hoved{
     
@@ -6,26 +7,42 @@ class Hoved{
         Spiller en = new Spiller('X');
         Spiller to = new Spiller('O');
         Scanner sc = new Scanner(System.in);
+        ArrayList<Character> markertX = new ArrayList<>();
+        ArrayList<Character> markertO = new ArrayList<>();
         en.tur = true;
         for (int i = 0; i < 3; i++){
             spillet[i] = new Spill();
             }
-
+            
         int teller1 = 0; 
         int teller2 = 0;
+        System.out.println();
+        System.out.println(" ! Velkommen til tre på rad ! ");
+        System.out.println();
+        System.out.println("*For å sette din markering velger du et tall for rad fra 0-2 og deretter index 0-2");
+        System.out.println();
         while(en.harVunnet() == false && to.harVunnet() == false){
             
             if(en.hentTur()){
-                System.out.print(" Spiller 1 sin tur!");
-                spillet[sc.nextInt()].grid[sc.nextInt()] = en.settMarkering();
+                System.out.println();
+                System.out.println(" Spiller 1 sin tur! velg tall: ");
+                System.out.print("Rad: ");
+                if(markertX.size() < 4){
+                    spillet[sc.nextInt()].grid[sc.nextInt()] = en.settMarkering();
+                }
+                
                 to.tur = true;
                 en.tur = false;
+                
             } else if(to.hentTur()){
-                System.out.print(" Spiller 2 sin tur!");
-                spillet[sc.nextInt()].grid[sc.nextInt()] = to.settMarkering();
+                System.out.println();
+                System.out.print(" Spiller 2 sin tur! velg tall: ");
+                if(markertO.size() < 4){
+                    spillet[sc.nextInt()].grid[sc.nextInt()] = to.settMarkering();
+                }
                 en.tur = true;
                 to.tur = false;
-
+                
             }
             for(Spill s: spillet){
                 System.out.println(s.grid);
@@ -33,13 +50,16 @@ class Hoved{
                     char nåværende = s.grid[i];
                     if(nåværende == en.merke){
                         teller1 ++;
+                        markertX.add(en.merke);
+                    } else if( nåværende == to.merke){
+                        teller2++;
+                        markertO.add(to.merke);
                     }
                 }
                     
                 }
-            if(teller1 == 3){
-                en.vant();
-            }
+                
+            
             }
             
             }
