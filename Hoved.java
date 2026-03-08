28b2d7c20f5d6d2814b0387b8dbdde7b595c3e3a
Aleksander <Alringstad@outlook.com>
2026-03-08 15:53:05 +0100

    Ferdig for nå

--- Hoved.java
+++ Hoved.java
@@ -1,21 +1,50 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 class Hoved{
+    
+    public static boolean sjekkHorisontalt(Spill[] spillet, char merke){
+        for(Spill s : spillet){
+            if(s.grid[0] == merke && s.grid[1] == merke && s.grid[2] == merke){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public static boolean sjekkVertikalt(Spill[] spillet, char merke){
+        for(int kolonne = 0; kolonne < 3; kolonne++){
+            if(spillet[0].grid[kolonne] == merke && spillet[1].grid[kolonne] == merke && spillet[2].grid[kolonne] == merke){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public static boolean sjekkDiagonalt(Spill[] spillet, char merke){
+    
+        if(spillet[0].grid[0] == merke && spillet[1].grid[1] == merke && spillet[2].grid[2] == merke){
+            return true;
+        }
+        
+        if(spillet[0].grid[2] == merke && spillet[1].grid[1] == merke && spillet[2].grid[0] == merke){
+            return true;
+        }
+        return false;
+    }
+    
+    public static boolean harVunnetSpillet(Spill[] spillet, char merke){
+        return sjekkHorisontalt(spillet, merke) || sjekkVertikalt(spillet, merke) || sjekkDiagonalt(spillet, merke);
+    }
     
     public static void main(String[] args) {
         Spill[]spillet = new Spill[3];
         Spiller en = new Spiller('X');
         Spiller to = new Spiller('O');
         Scanner sc = new Scanner(System.in);
-        ArrayList<Character> markertX = new ArrayList<>();
-        ArrayList<Character> markertO = new ArrayList<>();
         en.tur = true;
         for (int i = 0; i < 3; i++){
             spillet[i] = new Spill();
             }
             
-        int teller1 = 0; 
-        int teller2 = 0;
         System.out.println();
         System.out.println(" ! Velkommen til tre på rad ! ");
         System.out.println();
@@ -29,6 +58,10 @@ class Hoved{
                 System.out.print("Rad: ");
                 spillet[sc.nextInt()].grid[sc.nextInt()] = en.settMarkering();
                 
+                if(harVunnetSpillet(spillet, en.merke)){
+                    en.vant();
+                }
+                
                 to.tur = true;
                 en.tur = false;
                 
@@ -36,37 +69,26 @@ class Hoved{
                 System.out.println();
                 System.out.print(" Spiller 2 sin tur! velg tall: ");
                 spillet[sc.nextInt()].grid[sc.nextInt()] = to.settMarkering();
+                
+                if(harVunnetSpillet(spillet, to.merke)){
+                    to.vant();
+                }
+                
                 en.tur = true;
                 to.tur = false;
                 
             }
+            
             for(Spill s: spillet){
                 System.out.println(s.grid);
-                char nåværende;
-                for(int i = 0; i < s.grid.length; i++){
-                    nåværende = s.grid[i];
-                    if(nåværende == en.merke){
-                        teller1 ++;
-                        markertX.add(en.merke);
-                        if(s.grid[0] == en.merke && s.grid[1] == en.merke && s.grid[2] == en.merke){
-                            en.vant();
-                        }
-                        
-                    } else if( nåværende == to.merke){
-                        teller2++;
-                        markertO.add(to.merke);
-                        if(s.grid[0] == to.merke && s.grid[1] == to.merke && s.grid[2] == to.merke){
-                            to.vant();
-                        }
-                    }
-                    
                 }
-                       
-                }
+            if(en.vunnet == true){
+                System.out.print("Spiller 1 vant!");
+            } else if(to.vunnet == true){
+                System.out.print("Spiller 2 vant!");
+            }
             
             }
-            markertX.clear();
-            markertO.clear();
             }
         }
         
